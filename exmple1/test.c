3f8600e2f88215ed74193697c22c2e2379c7aa08
hounati <hounatiahmed@gmail.com>
2023-09-13 09:16:13 +0100

    test

--- exmple1/test.c
+++ exmple1/test.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-  printf("Hello World!");//oplaa
+  int N , S , Z ;
+  printf("veullez entrer la valeur de N : ");
+  scanf("%d",&N);
+  printf("veullez entrer la valeur de S : ");
+  scanf("%d",&S);
+  printf("la valeur de Z est : %d ",N + S);  
   
   return 0;
 }
